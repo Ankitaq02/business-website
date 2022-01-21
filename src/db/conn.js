f65ec2e1e9b5ce5cd0e80d5d90950b015ac2d260
Ankita <ankita.babita777@gmail.com>
2022-01-21 22:44:50 +0530

    homepage

---
+++ src/db/conn.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/mydata");
+
+
+// useCreateIndex:true,
+    // useNewUrlParser:true,
+    // useUnifiedTopology:true
