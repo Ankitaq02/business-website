844d82b236ae666663ee91f2790866604c69f44a
Ankita <ankita.babita777@gmail.com>
2022-01-22 11:38:06 +0530

    styling

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const hbs=require("hbs");
 require("./db/conn");
 
 const app=express();
-const port=process.env.PORT||3000;
+const port=process.env.PORT||3001;
 
 const staticpath=path.join(__dirname,"../public");
 const templatepath=path.join(__dirname,"../templates/views");
@@ -20,9 +20,30 @@ app.set("view engine","hbs");
 app.set("views",templatepath);
 hbs.registerPartials(partialpath);
 
+app.get("/contact",(req,res)=>{
+    res.render("index");
+})
 app.get("/",(req,res)=>{
     res.render("index");
 })
+
+app.post("/contact", async (req, res) => {
+    try {
+      const registerEmployee = new Register({
+        name: req.body.name,
+        
+        email: req.body.email,
+        password: req.body.password,
+        message:req.body.text,
+      })
+  
+      const registered = await registerEmployee.save();
+      res.status(201).render("index");
+  
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
 
 app.listen(port,()=>{
     console.log(`server running at ${port}`);
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,31 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/mydata");
+mongoose.connect("mongodb://localhost:27017/mystructure");
 
+const SeopleSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+})
 
-// useCreateIndex:true,
-    // useNewUrlParser:true,
-    // useUnifiedTopology:true
+const PeopleId=mongoose.model("PeopleId",SeopleSchema);
+
+var anki=new PeopleId({name:'anki'});
+
+anki.save(function(err,anki){
+    if(err)return console.error(err);
+})
+module.exports=PeopleId;
